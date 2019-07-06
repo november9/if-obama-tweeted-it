806ab91faa34859c4b3f1c0e238a348af81ddf50
Doron Orenstein <jazziseverywhere@gmail.com>
2019-07-06 09:45:03 -0500

    updated files

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ In the project directory, you can run:
 
 ### `npm start`
 
+<<<<<<< HEAD
 Runs the app in the development mode.<br>
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
@@ -66,3 +67,6 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+=======
+based off of https://www.codementor.io/olatundegaruba/nodejs-restful-apis-in-10-minutes-q0sgsfhbd
+>>>>>>> updated files
--- api/controllers/iotiController.js
+++ api/controllers/iotiController.js
@@ -3,6 +3,7 @@
 var mongoose = require("mongoose");
 var Tweet = mongoose.model("Tweets");
 var Twit = require("twit");
+<<<<<<< HEAD
 var configure = require("../../config");
 
 var T = new Twit(configure);
@@ -53,11 +54,85 @@ exports.list_all_tweets = function(req, res) {
 exports.create_a_tweet = function(req, res) {
     var new_tweet = new Tweet(req.body);
     new_tweet.save(function(err, tweet) {
+=======
+var configure = require("../../configure");
+
+var T = new Twit(configure);
+var lastTrumpTweet;
+
+function tweetTrump () {
+
+    T.get(
+        "statuses/user_timeline",
+        { screen_name: "realDonaldTrump", count: 10 },
+        function(err, data, response) {
+            // console.log('data', data)
+            lastTrumpTweet = data[9].id;
+            return data.map(function(val) {
+                console.log('lastTrumpTweet', lastTrumpTweet);
+                console.log('val.id', val.id);
+                if (lastTrumpTweet === val.id) {
+                    return;
+                } else {
+                    T.post('statuses/update', {status: val.text});
+                }
+            });
+        }
+    );
+}
+
+// tweetTrump();
+
+// T.get(
+//     "statuses/user_timeline",
+//     { screen_name: "realDonaldTrump", count: 10 },
+//     function(err, data, response) {
+//         // console.log('data', data)
+//         return data.map(function(val) {
+//             // T.post('statuses/update', {status: val.text});
+//         });
+//     }
+// );
+
+// var scrape_tweets_from_twitter = function() {
+//      T.get(
+//         "statuses/user_timeline",
+//         { screen_name: "realDonaldTrump", count: 10 },
+//         function(err, data, response) {
+//             // console.log('data', data)
+//             return data.map(function(val) {
+//                 console.log("TWEET TWEET!:  ", val.text);
+//                 responseData(response, {
+//                     statuses: response
+//                 });
+
+//                 // return val.text;
+//             });
+//         }
+//     );
+// };
+
+// scrape_tweets_from_twitter();
+
+exports.list_all_tweets = function(req, res) {
+    Tweet.find({}, function(err, tweet) {
+>>>>>>> updated files
         if (err) res.send(err);
         res.json(tweet);
     });
 };
 
+<<<<<<< HEAD
+=======
+exports.create_a_tweet = function(req, res) {
+    var new_tweet = new Tweet(req.body);
+    new_tweet.save(function(err, tweet) {
+        if (err) res.send(err);
+        res.json(tweet);
+    });
+};
+
+>>>>>>> updated files
 exports.read_a_tweet = function(req, res) {
     Tweet.findById(req.params.tweetId, function(err, tweet) {
         if (err) res.send(err);

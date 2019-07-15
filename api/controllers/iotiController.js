5ef677376193e8572f7b400c89c3ae2778e0fd03
Doron Orenstein <jazziseverywhere@gmail.com>
2019-07-15 12:51:23 -0500

    switched config stuff

--- api/controllers/iotiController.js
+++ api/controllers/iotiController.js
@@ -3,9 +3,15 @@
 var mongoose = require("mongoose");
 var Tweet = mongoose.model("Tweets");
 var Twit = require("twit");
-var configure = require("../../configure");
 
-var T = new Twit(configure);
+var T = new Twit({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL: false  // optional - requires SSL certificates to be valid.
+});
 var lastTrumpTweet;
 
 function tweetTrump () {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+require('dotenv').config();
 
 var express = require('express'),
   app = express(),
